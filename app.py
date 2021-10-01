ecd9eda50cbdd13cddbb4b1f2f0e4736ede9666f
Giulio <giulio.umbrella@gmail.com>
2021-10-01 17:53:11 +0200

    Define function for subtraction check

--- app.py
+++ app.py
@@ -11,9 +11,28 @@ def analyze_func(func_name, m,n):
     seconds = toc - tic
     print(f"{func_name.__name__.capitalize()}\t -> Elapsed time: {seconds:.5f}")
 
-m = cambio_di_base(int(sys.argv[1]))
-n = cambio_di_base(int(sys.argv[2]))
+def test_values(a,b,test_values):
 
+    x = [randint(a+1, b) for i in range(test_values)]
+    y = [randint(1, a) for i in range(test_values)]
+
+    numb_err = 0
+    for i,j in zip(x,y):
+
+        diff_base_10 = i-j
+        diff_base_2 = binary_subtraction(cambio_di_base(i),
+        cambio_di_base(j))
+        diff_base_10_check = base_2_to_10(diff_base_2)
+
+        if diff_base_10 != diff_base_10_check:
+            numb_err += 1
+
+    print('total number of error',numb_err)
+
+# test_values(1000, 10000, 5000)
+
+
+analyze_func(binary_subtraction, cambio_di_base(99999999), cambio_di_base(99999))
 
 #### 1 TIME COMPLEXITY #####
 
@@ -29,12 +48,46 @@ n = cambio_di_base(int(sys.argv[2]))
 # print(partial_sums)
 
 #### 3 PRODUCT ####
-num_values = 10
-max_value  = 10
-list_values = randint(num_values, max_value)
-x =
-for i in list_values:
+# num_values = 10
+# max_value  = 10
+# list_values = [cambio_di_base(randint(1, max_value)) for i in range(num_values)]
+#
+# partial_product = list_values[0]
+# for i in range(1,len(list_values)):
+#     partial_product = binary_product(partial_product, list_values[i])
+#
+# print(partial_product)
 
-
+#### SUBTRACTION ####
+# from random import randint
+#
+# x = [randint(100001, 5000000) for i in range(10000)]
+# y = [randint(1, 10000) for i in range(10000)]
+#
+# numb_err = 0
+# for i,j in zip(x,y):
+#
+#     diff_base_10 = i-j
+#     diff_base_2 = binary_subtraction(cambio_di_base(i),
+#     cambio_di_base(j))
+#     diff_base_10_check = base_2_to_10(diff_base_2)
+#
+#     if diff_base_10 != diff_base_10_check:
+#         numb_err += 1
+#
+# print('total number of error',numb_err)
+    # else:
+    #     print(f"{i} - {j} OK")
+# m = cambio_di_base(int(sys.argv[1]))
+# n = cambio_di_base(int(sys.argv[2]))
+# print(int(sys.argv[1]), m)
+# print(int(sys.argv[2]), n)
+#
+#
+# x = binary_subtraction(m,n)
+# print(x)
+# print(base_2_to_10(x))
+# print(str(int(sys.argv[1])-int(sys.argv[2])))
+# print(cambio_di_base(int(sys.argv[1])-int(sys.argv[2])))
 
 #### MODULAR PRODUCT ####

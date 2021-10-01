4d1de7148e0f362c25e73bce3212272efeb42a0d
Giulio <giulio.umbrella@gmail.com>
2021-10-01 10:38:42 +0200

    First attempt of binary subtraction

--- binary_ops.py
+++ binary_ops.py
@@ -1,4 +1,10 @@
-import sys
+from random import randint
+
+def set_longer_shorter(x,y):
+    if len(x) > len(y):
+        return x,y
+    else:
+        return y,x
 
 def base_2_to_10(base_2_list):
     x = 0
@@ -48,21 +54,21 @@ def binary_sum(x,y):
     '111':(1,1),
     }
 
-    ops = 0
+    # ops = 0
     carry = 0
     for i in range(len(m))[::-1]:
         bit_sum = str(m[i]) + str(n[i]) + str(carry)
         # bit_key = ''.join(map(str, [m[i],n[i],carry]))
         value, carry = sum_result[bit_sum]
         sum_list.insert(0,value)
-        ops += 1
+        # ops += 1
 
     if carry == 1:
         sum_list.insert(0,1)
-        ops += 1
+        # ops += 1
 
 
-    return ops, sum_list
+    return sum_list
 
 def binary_product(x,y):
 
@@ -71,11 +77,11 @@ def binary_product(x,y):
     else:
         m, n = y,x
 
-    ops = 0
+    # ops = 0
     shifts = []
     list_addends = []
     for i in n[::-1]:
-        ops += 1
+        # ops += 1
         if i == 1:
             list_addends.append(m+shifts)
         shifts.append(0)
@@ -84,11 +90,49 @@ def binary_product(x,y):
     partial_sums = list_addends[0]
 
     while i < len(list_addends):
-        x, partial_sums = binary_sum(partial_sums,list_addends[i])
-        ops += x
+        # x, partial_sums = binary_sum(partial_sums,list_addends[i])
+        partial_sums = binary_sum(partial_sums,list_addends[i])
+        # ops += x
         i += 1
 
-    return ops, list_addends, partial_sums
+    return partial_sums
+    # return ops, list_addends, partial_sums
+
+def binary_subtraction(x,y):
+    m,n = set_longer_shorter(x,y)
+    n = append_leading_zeros(m,n)
+
+    print(m)
+    print(n)
+
+    result_list = []
+    i = len(m)-1
+    while i > -1 :
+        bit_pair = str(m[i]) + str(n[i])
+        sub_res = {'00':0,'10':1,'11':0}
+        if bit_pair in sub_res:
+            result_list.insert(0,sub_res[bit_pair])
+        # else:
+        #     while(m[i] == 0):
+        #         i -= 1
+        i -= 1
+
+    return result_list
 
 def binary_division(m,n):
+    pass
+
+def sequence_product():
+    pass
+# num_values = 10
+# max_value  = 10
+# list_values = [cambio_di_base(randint(1, max_value)) for i in range(num_values)]
+#
+# partial_product = list_values[0]
+# for i in range(1,len(list_values)):
+#     partial_product = binary_product(partial_product, list_values[i])
+#
+# print(partial_product)
+
+def sequence_product_modular():
     pass

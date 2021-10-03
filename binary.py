803226d8576c0d310060f00f98c320b4c122535a
Giulio <giulio.umbrella@gmail.com>
2021-10-03 09:52:13 +0200

    Add binary addition override
    * Still need to sort longer and shorter values

--- binary.py
+++ binary.py
@@ -1,14 +1,43 @@
 class Binary:
 
-    def __init__(self, numb)
+    def __init__(self, numb):
         r_list = []
 
         x = numb
         while x > 0:
-            r = x % base
+            r = x % 2
             q = (x - r)//2
             x = q
             r_list.append(r)
 
         r_list = r_list[::-1]
-        self.vallue = r_list
+        self.value = r_list
+
+    def __str__(self):
+        return str(''.join(self.value))
+
+
+    def __add__(self,other):
+        m = self.value
+        n = self.value
+
+        sum_list = []
+        three_bits_sum = {
+        '000':(0,0),'010':(1,0),'100':(1,0),'110':(0,1),
+        '001':(1,0),'011':(0,1),'101':(0,1),'111':(1,1),
+        }
+
+        carry = 0
+        for i in range(len(m))[::-1]:
+            bit_sum = str(m[i]) + str(n[i]) + str(carry)
+            value, carry = three_bits_sum[bit_sum]
+            sum_list.insert(0,value)
+
+        if carry == 1:
+            sum_list.insert(0,1)
+
+        return sum_list
+
+x = Binary(1)
+y = Binary(1)
+print(x+y)

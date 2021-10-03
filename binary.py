1ec29d2d3941e986e053c123c68b0fadd0dd9e5e
Giulio <giulio.umbrella@gmail.com>
2021-10-03 09:36:02 +0200

    Add constructor

--- binary.py
+++ binary.py
@@ -1,2 +1,14 @@
 class Binary:
-    pass
+
+    def __init__(self, numb)
+        r_list = []
+
+        x = numb
+        while x > 0:
+            r = x % base
+            q = (x - r)//2
+            x = q
+            r_list.append(r)
+
+        r_list = r_list[::-1]
+        self.vallue = r_list

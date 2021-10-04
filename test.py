5131d4558532cefa1db523fba5855ba6f7aa387c
Giulio <giulio.umbrella@gmail.com>
2021-10-04 16:38:35 +0200

    Add other results to be tested

---
+++ test.py
@@ -0,0 +1,17 @@
+x = [0]
+print(remove_leading_zeros(x))
+
+y = [0,0,1]
+print(remove_leading_zeros(y))
+
+z = [1]
+print(remove_leading_zeros(z))
+
+
+m = base_10_to_base_n(27)
+n = base_10_to_base_n(3)
+print(m)
+print(n)
+print(binary_product(m,n))
+print(binary_division(m,n))
+print(binary_mod(m,n))

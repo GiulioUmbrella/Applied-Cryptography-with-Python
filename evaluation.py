aa03a9bd642ad2d51bb6280a6dae69fd578cacc7
Giulio <giulio.umbrella@gmail.com>
2021-10-04 12:04:54 +0200

    Minor changes

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,9 @@
+import sys
 from binary_ops import *
 
-m = int(sys.argv[1])
-n = int(sys.argv[2])
+m = 2**10000 -1
+n = 2**16 -1
 
-print(cambio_di_base(m*n))
+analyze_func(binary_addition, base_10_to_base_n(m), base_10_to_base_n(n))
+analyze_func(binary_product,  base_10_to_base_n(m), base_10_to_base_n(n))
+analyze_func(binary_division, base_10_to_base_n(m), base_10_to_base_n(n))

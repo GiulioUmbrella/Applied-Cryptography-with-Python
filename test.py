7f22a73d15d4cd10ea9722df17f277013d60258c
Giulio <giulio.umbrella@gmail.com>
2021-10-05 10:29:54 +0200

    Move test function

--- test.py
+++ test.py
@@ -15,3 +15,22 @@ print(n)
 print(binary_product(m,n))
 print(binary_division(m,n))
 print(binary_mod(m,n))
+
+
+def test_values(a,b,test_values):
+
+    x = [randint(a+1, b) for i in range(test_values)]
+    y = [randint(1, a) for i in range(test_values)]
+
+    numb_err = 0
+    for i,j in zip(x,y):
+
+        diff_base_10 = i-j
+        diff_base_2 = binary_subtraction(base_10_to_base_n(i),
+        base_10_to_base_n(j))
+        diff_base_10_check = base_2_to_10(diff_base_2)
+
+        if diff_base_10 != diff_base_10_check:
+            numb_err += 1
+
+    print('total number of error',numb_err)

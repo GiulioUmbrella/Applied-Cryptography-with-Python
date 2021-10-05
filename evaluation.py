2957661fb99a9e8d0c7d3fbdf1e4cd12688482c7
Giulio <giulio.umbrella@gmail.com>
2021-10-05 10:30:26 +0200

    Move evaluation function

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,15 @@
 import sys
 from binary_ops import *
 
+def analyze_func(func_name, m,n):
+    tic = time.time()
+    func_name(m,n)
+    toc = time.time()
+    seconds = toc - tic
+    print(f"{func_name.__name__.capitalize()}\t -> Elapsed time: {seconds:.5f}")
+
+
+
 m = 2**10000 -1
 n = 2**16 -1
 
